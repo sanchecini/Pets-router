69fb41c98b4724c78357b2c30f901de8dda33518
Sanchecini <ansaindeep@gmail.com>
2023-06-08 04:45:56 -0600

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import ListCategories from "../components/ListCategories"
+import ListPosts from "../components/ListPosts"
+
+const Home = () => {
+  return (
+    <main>
+      <div className="container">
+        <h2 className="title-page">Pet noticias</h2>
+      </div>
+      <ListCategories/>
+      <ListPosts url={"/posts"} />
+    </main>
+  )
+}
+
+export default Home
